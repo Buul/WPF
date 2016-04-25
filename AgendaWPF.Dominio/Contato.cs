5ad94e52fb10be0c69e88bdb164b28c7f16b3c4e
Paulo Firmino <paulo.firmino2@gmail.com>
2016-04-25 16:10:20 -0300

    ic

---
+++ AgendaWPF.Dominio/Contato.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWPF.Dominio
+{
+    public class Contato : Entidade
+    {
+        private string _nome = string.Empty;
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value;
+            }
+        }
+        private IList<Telefone> _telefones = new List<Telefone>();
+        public IList<Telefone> Telefones
+        {
+            get { return _telefones; }
+            set
+            {
+                _telefones = value;
+            }
+        }
+    }
---
+++ AgendaWPF.Dominio/Telefone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWPF.Dominio
+{
+    public class Telefone
+    {
+        private TipoTelefoneEnum _tipo = TipoTelefoneEnum.Residencial;
+
+        public TipoTelefoneEnum Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+
+        private string _numero = string.Empty;
+
+        public string Numero
+        {
+            get
+            {
+                return _numero;
+                
+            }
+            set
+            {
+                _numero = value;
+            }
+        }
+    }
+    public enum TipoTelefoneEnum
+    {
+        Residencial,
+        Comercial,
+        Celular,
+        Recado
+    }
+}
